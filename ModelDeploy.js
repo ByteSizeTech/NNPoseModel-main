231196a58518f3ee7a61c246b9e88b9cfa8bcc31
Nimra Gauhar <53583132+NimraGJay@users.noreply.github.com>
2020-12-03 09:41:27 +0500

    slight changes

--- ModelDeploy.js
+++ ModelDeploy.js
@@ -22,23 +22,27 @@ function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on("pose", gotPoses);
 
-  let options = {
+  let posenetOpts = {
     architecture: "ResNet50",
-    imageScaleFactor: 0.3,
-    outputStride: 16,
-    flipHorizontal: false,
+    // imageScaleFactor: 0.3,
+    outputStride: 32,
+    // flipHorizontal: false,
     minConfidence: 0.5,
     maxPoseDetections: 1,
+    minPartConfidence:0.5,
     scoreThreshold: 0.5,
     nmsRadius: 20,
     detectionType: "single",
-    inputResolution: 513,
+    inputResolution: 256,
     multiplier: 0.75,
     quantBytes: 2,
   };
+
+  poseNet = ml5.poseNet(video,posenetOpts, modelLoaded);
+  poseNet.on("pose", gotPoses);
+
+ 
   brain = ml5.neuralNetwork(options);
   // PUSHUP MODEL
   const modelInfo = {
